0e49a12d38b1c3a5de9332507608b2c0c3f17ad8
Leonardo Giordani <giordani.leonardo@gmail.com>
2024-02-13 15:59:54 +0000

    A simple list of elements

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::collections::HashMap;
 
 #[derive(Parser)]
 struct Cli {
@@ -6,8 +7,33 @@ struct Cli {
     key: String,
 }
 
+struct TodoList {
+    // true = to do, false = done
+    items: HashMap<String, bool>,
+}
+
+impl TodoList {
+    fn new() -> TodoList {
+        let items: HashMap<String, bool> = HashMap::new();
+
+        TodoList { items: items }
+    }
+}
+
 fn main() {
     let args = Cli::parse();
 
+    let todo = TodoList::new();
+
     println!("Command line: {} {}", args.command, args.key);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn init_todo() {
+        let todo = TodoList::new();
+    }
 }
