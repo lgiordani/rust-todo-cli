89cccf4d64c7c998b6f254d3af2f68557e58759b
Leonardo Giordani <giordani.leonardo@gmail.com>
2024-02-13 16:07:19 +0000

    Exposing commands on the CLI

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 #[derive(Parser)]
 struct Cli {
     command: String,
-    key: String,
+    key: Option<String>,
 }
 
 struct TodoList {
@@ -44,9 +44,50 @@ impl TodoList {
 fn main() {
     let args = Cli::parse();
 
-    let todo = TodoList::new();
+    let mut todo = TodoList::new();
 
-    println!("Command line: {} {}", args.command, args.key);
+    todo.add("Something to do".to_string());
+    todo.add("Something else to do".to_string());
+    todo.add("Something done".to_string());
+    todo.mark("Something done".to_string(), false).unwrap();
+
+    let result = match args.command.as_str() {
+        "add" => match args.key {
+            Some(key) => {
+                todo.add(key);
+                Ok(())
+            }
+            None => Err("Key cannot be empty!".to_string()),
+        },
+        "mark-done" => match args.key {
+            Some(key) => todo
+                .mark(key, false)
+                .map_err(|e| format!("Invalid key {}", e))
+                .and(Ok(())),
+            None => Err("Key cannot be empty!".to_string()),
+        },
+        "list" => {
+            let (todo_items, done_items) = todo.list();
+
+            println!("# TO DO");
+            println!();
+            todo_items.for_each(|x| println!(" * {}", x));
+
+            println!();
+
+            println!("# DONE");
+            println!();
+            done_items.for_each(|x| println!(" * {}", x));
+
+            Ok(())
+        }
+        cmd => Err(format!("Command {} not recognised", cmd)),
+    };
+
+    match result {
+        Err(e) => println!("ERROR: {}", e),
+        Ok(_) => println!("SUCCESS"),
+    }
 }
 
 #[cfg(test)]
