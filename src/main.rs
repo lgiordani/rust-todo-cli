e0ee0b9fdb14e2d7f4d0a5022718d5c7923249e6
Leonardo Giordani <giordani.leonardo@gmail.com>
2024-02-13 16:04:05 +0000

    Multiple additions and updates

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 
 #[derive(Parser)]
@@ -20,7 +21,9 @@ impl TodoList {
     }
 
     fn add(&mut self, key: String) {
-        self.items.insert(key, true);
+        if let Entry::Vacant(entry) = self.items.entry(key) {
+            entry.insert(true);
+        }
     }
 }
 
@@ -46,5 +49,28 @@ mod tests {
         let mut todo = TodoList::new();
         todo.add(String::from("Something to do"));
         assert_eq!(todo.items.get("Something to do"), Some(&true))
+    }
+
+    #[test]
+    fn add_item_already_exist() {
+        let mut todo = TodoList::new();
+        todo.add(String::from("Something to do"));
+        todo.add(String::from("Something to do"));
+        assert_eq!(todo.items.get("Something to do"), Some(&true));
+        assert_eq!(todo.items.len(), 1);
+    }
+
+    #[test]
+    fn add_item_does_not_change_value() {
+        let mut todo = TodoList::new();
+        todo.add(String::from("Something to do"));
+
+        if let Some(x) = todo.items.get_mut("Something to do") {
+            *x = false;
+        }
+
+        todo.add(String::from("Something to do"));
+        assert_eq!(todo.items.get("Something to do"), Some(&false));
+        assert_eq!(todo.items.len(), 1);
     }
 }
